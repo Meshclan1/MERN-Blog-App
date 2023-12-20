7ec7d65613fb2710437ab7d1680693967f8bc2e9
Ayo <Ayo.meshe@gmail.com>
2023-12-20 16:56:29 +0000

    initial commit and bug fixes from previous attempt

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import IndexPage from "./components/IndexPage";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
