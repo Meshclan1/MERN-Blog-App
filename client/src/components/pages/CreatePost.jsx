1e373d4ad1770f49ac732c6c6c099b1e23bc362d
Ayo <Ayo.meshe@gmail.com>
2023-12-22 16:31:50 +0000

    updating with reactQuill

--- client/src/components/pages/CreatePost.jsx
+++ client/src/components/pages/CreatePost.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
   return (
     <form>
       <input type="title" placeholder={"Title"}></input>
       <input type="summary" placeholder={"Summary"}></input>
       <input type="file"></input>
-      <ReactQuill theme="snow" />
+      <ReactQuill value={content} module={modules} formats={formats} />
+      <button style={{ marginTop: "5px" }}>Create Post</button>
     </form>
   );
 }
