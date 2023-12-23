5f928345eb720dc92e5e5520044d71f9e180741f
Ayo <Ayo.meshe@gmail.com>
2023-12-23 17:16:23 +0000

    implementing edit-post page

--- client/src/components/pages/PostPage.jsx
+++ client/src/components/pages/PostPage.jsx
@@ -1,9 +1,13 @@
 import { formatISO9075 } from "date-fns";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { UserContext } from "../userContext";
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
+
+  const { userInfo } = useContext(UserContext);
+
   const { id } = useParams();
   useEffect(() => {
     fetch(`http://localhost:4000/post/${id}`).then((res) => {
@@ -19,7 +23,14 @@ export default function PostPage() {
     <div className="post-page">
       <h1>{postInfo.title}</h1>
       <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-      <div className="author">By {postInfo.author.username}</div>
+      <div className="author">By @{postInfo.author.username}</div>
+      {userInfo.id === postInfo.author._id && (
+        <div className="edit-row">
+          <a className="edit-btn" href="">
+            Edit this post
+          </a>
+        </div>
+      )}
       <div className="image">
         <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
       </div>
