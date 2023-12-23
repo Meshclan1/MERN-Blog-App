7086c3a7ce184c3513d8b10fa0f651832e129d81
Ayo <Ayo.meshe@gmail.com>
2023-12-23 14:38:26 +0000

    Linking header & images to post id

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { formatISO9075 } from "date-fns";
+import { Link } from "react-router-dom";
 
 export default function Post({
   title,
@@ -6,15 +7,20 @@ export default function Post({
   cover,
   content,
   createdAt,
+  _id,
   author,
 }) {
   return (
     <div className="post">
       <div className="image">
-        <img src={"http://localhost:4000/" + cover} alt="Brazil"></img>
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:4000/" + cover} alt="Brazil"></img>
+        </Link>
       </div>
       <div className="texts">
-        <h2>{title}</h2>
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className="info">
           <a className="author">{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
